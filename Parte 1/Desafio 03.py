3bae7e39c549d8acfa5dde15b7e3ad682c79c327
juniordm20 <juniordcesar@gmail.com>
2025-08-04 11:43:05 -0300

    Desafio 3 do curso de python do curso em video

---
+++ Parte 1/Desafio 03.py
@@ -0,0 +1,13 @@
+print("======== DESAFIO 3 =========")
+NUM1 = float(input("Informe o primeiro numero:"))
+NUM2 = float(input("Informe o segundo numero:"))
+
+Soma = NUM1+NUM2
+subt = NUM1 - NUM2
+DIV = NUM1/NUM2
+MULT = NUM1*NUM2
+
+print("A soma do numero",NUM1," mais o numero",NUM2,"é",Soma)
+print("A subtraçao do numero",NUM1," menos o numero",NUM2,"é",subt)
+print("A divisao do numero",NUM1," e o numero",NUM2,"é",DIV)
+print("A multiplicação do numero",NUM1," e o numero",NUM2,"é",MULT)
